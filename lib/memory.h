0d091beb17fc7d67691d1ff54fd530b79a98c4fe
David Lamparter <equinox@opensourcerouting.org>
2015-05-29 11:26:37 +0200

    lib: more compatibility...

--- lib/memory.h
+++ lib/memory.h
@@ -41,6 +41,31 @@ struct memgroup {
 	const char *name;
 };
 
+#if defined(__clang__)
+# if __clang_major__ > 3 || (__clang_major__ == 3 && __clang_minor__ >= 5)
+#  define _RET_NONNULL  , returns_nonnull
+# endif
+# define _CONSTRUCTOR(x) constructor(x)
+#elif defined(__GNUC__)
+# if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 9)
+#  define _RET_NONNULL  , returns_nonnull
+# endif
+# if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 3)
+#  define _CONSTRUCTOR(x) constructor(x)
+#  define _ALLOC_SIZE(x)  alloc_size(x)
+# endif
+#endif
+
+#ifndef _RET_NONNULL
+# define _RET_NONNULL
+#endif
+#ifndef _CONSTRUCTOR
+# define _CONSTRUCTOR(x) constructor
+#endif
+#ifndef _ALLOC_SIZE
+# define _ALLOC_SIZE(x)
+#endif
+
 /* macro usage:
  *
  *  mydaemon.h
@@ -72,15 +97,14 @@ struct memgroup {
 	struct memgroup _mg_##mname \
 	__attribute__ ((section (".data.mgroups"))) = { \
 		.name = desc, \
-		.types = NULL, .next = NULL, \
+		.types = NULL, .next = NULL, .insert = NULL, \
 	}; \
 	static void _mginit_##mname (void) \
-	  __attribute__ ((constructor (1000))); \
+	  __attribute__ ((_CONSTRUCTOR (1000))); \
 	static void _mginit_##mname (void) \
 	{	extern struct memgroup **mg_insert; \
 		*mg_insert = &_mg_##mname; \
-		mg_insert =  &_mg_##mname.next; \
-		_mg_##mname.insert = &_mg_##mname.types; }
+		mg_insert =  &_mg_##mname.next; }
 
 
 #define DECLARE_MTYPE(name) \
@@ -94,9 +118,11 @@ struct memgroup {
 		.next = NULL, .n_alloc = 0, .size = 0, \
 	}; \
 	static void _mtinit_##mname (void) \
-	  __attribute__ ((constructor (1001))); \
+	  __attribute__ ((_CONSTRUCTOR (1001))); \
 	static void _mtinit_##mname (void) \
-	{	*_mg_##group.insert = &_mt_##mname; \
+	{	if (_mg_##group.insert == NULL) \
+			_mg_##group.insert = &_mg_##group.types; \
+		*_mg_##group.insert = &_mt_##mname; \
 		_mg_##group.insert =  &_mt_##mname.next; }
 
 #define DEFINE_MTYPE(group, name, desc) \
@@ -106,30 +132,15 @@ struct memgroup {
 	static struct memtype * const MTYPE_ ## name = &_mt_##name;
 
 
-#if defined(__clang__)
-# if __clang_major__ > 3 || (__clang_major__ == 3 && __clang_minor__ >= 5)
-#  define _RET_NONNULL  , returns_nonnull
-# endif
-#elif defined(__GNUC__)
-# if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 9)
-#  define _RET_NONNULL  , returns_nonnull
-# endif
-#endif
-#ifndef _RET_NONNULL
-# define _RET_NONNULL
-#endif
-
 extern void *qmalloc (struct memtype *mt, size_t size)
-	__attribute__ ((malloc, alloc_size (2), nonnull (1) _RET_NONNULL));
+	__attribute__ ((malloc, _ALLOC_SIZE(2), nonnull (1) _RET_NONNULL));
 extern void *qcalloc (struct memtype *mt, size_t size)
-	__attribute__ ((malloc, alloc_size (2), nonnull (1) _RET_NONNULL));
+	__attribute__ ((malloc, _ALLOC_SIZE(2), nonnull (1) _RET_NONNULL));
 extern void *qrealloc (struct memtype *mt, void *ptr, size_t size)
-	__attribute__ ((alloc_size (3), nonnull (1) _RET_NONNULL));
+	__attribute__ ((_ALLOC_SIZE(3), nonnull (1) _RET_NONNULL));
 extern void *qstrdup (struct memtype *mt, const char *str)
 	__attribute__ ((malloc, nonnull (1) _RET_NONNULL));
 extern void qfree (struct memtype *mt, void *ptr);
-
-#undef _RET_NONNULL
 
 #if 0
 #define XMALLOC(mtype, size)		qmalloc(mtype, size)
