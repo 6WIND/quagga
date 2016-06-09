b5d43f84ac715b8f46e5d45f0122e64fa698e77b
Julien Courtat <julien.courtat@6wind.com>
2016-06-09 17:12:00 +0200

    qthrift: enable graceful restart flag in startBgp
    According to stalepathtime parameter value of startBgp(), graceful
    restart flag if set or not in bgp instance.
    No need to call enableGraceFulRestart() API to do so.
    
    Signed-off-by: Julien Courtat <julien.courtat@6wind.com>

--- qthriftd/qthrift_bgp_configurator.c
+++ qthriftd/qthrift_bgp_configurator.c
@@ -530,6 +530,10 @@ instance_bgp_configurator_handler_start_bgp(BgpConfiguratorIf *iface, gint32* _r
     inst.default_holdtime = holdTime;
     inst.default_keepalive= keepAliveTime;
     inst.stalepath_time = stalepathTime;
+    if(stalepathTime)
+      bgp_flag_set(&inst, BGP_FLAG_GRACEFUL_RESTART);
+    else
+      bgp_flag_unset(&inst, BGP_FLAG_GRACEFUL_RESTART);
     if (announceFbit == TRUE)
       bgp_flag_set(&inst, BGP_FLAG_GR_PRESERVE_FWD);
     else
