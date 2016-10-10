b04499cde7523ccf05cbb23c437fdd8533711210
Philippe Guibert <philippe.guibert@6wind.com>
2016-10-10 09:40:28 +0200

    bgpd: add vty command : show ipv6 bgp vrf [route]
    This command permits to dump IPv6 entries in VRF RIB filled by export RT facility.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- bgpd/bgp_route.c
+++ bgpd/bgp_route.c
@@ -8641,6 +8641,16 @@ DEFUN (show_ip_bgp_vrf,
   return bgp_show_vrf (vty, argv[0], AFI_IP, bgp_show_type_normal, NULL);
 }
 
+DEFUN (show_ipv6_bgp_vrf,
+       show_ipv6_bgp_vrf_cmd,
+       "show ipv6 bgp vrf WORD",
+       SHOW_STR
+       IPV6_STR
+       BGP_STR)
+{
+  return bgp_show_vrf (vty, argv[0], AFI_IP6, bgp_show_type_normal, NULL);
+}
+
 DEFUN (show_ip_bgp_ipv4,
        show_ip_bgp_ipv4_cmd,
        "show ip bgp ipv4 (unicast|multicast)",
@@ -8679,6 +8689,19 @@ DEFUN (show_ip_bgp_vrf_route,
        "Route Distinguisher\n")
 {
   afi_t afi = (strncmp (argv[1], "ipv6", 4) == 0) ? AFI_IP6 : AFI_IP;
+  return bgp_show_vrf_route (vty, argv[0], argv[2], afi, 0);
+}
+
+DEFUN (show_ipv6_bgp_vrf_route,
+       show_ipv6_bgp_vrf_route_cmd,
+       "show ipv6 bgp vrf WORD X:X::X:X",
+       SHOW_STR
+       IPV6_STR
+       BGP_STR
+       "VRF\n"
+       "Route Distinguisher\n")
+{
+  afi_t afi = AFI_IP6;
   return bgp_show_vrf_route (vty, argv[0], argv[2], afi, 0);
 }
 
@@ -18153,9 +18176,11 @@ bgp_route_init (void)
   /* old style commands */
   install_element (VIEW_NODE, &show_ip_bgp_cmd);
   install_element (VIEW_NODE, &show_ip_bgp_vrf_cmd);
+  install_element (VIEW_NODE, &show_ipv6_bgp_vrf_cmd);
   install_element (VIEW_NODE, &show_ip_bgp_ipv4_cmd);
   install_element (VIEW_NODE, &show_ip_bgp_route_cmd);
   install_element (VIEW_NODE, &show_ip_bgp_vrf_route_cmd);
+  install_element (VIEW_NODE, &show_ipv6_bgp_vrf_route_cmd);
   install_element (VIEW_NODE, &show_ip_bgp_ipv4_route_cmd);
   install_element (VIEW_NODE, &show_ip_bgp_vpnv4_all_route_cmd);
   install_element (VIEW_NODE, &show_bgp_l2vpn_evpn_all_route_cmd);
@@ -18241,6 +18266,7 @@ bgp_route_init (void)
   install_element (VIEW_NODE, &show_ip_bgp_view_rsclient_prefix_cmd);
   install_element (RESTRICTED_NODE, &show_ip_bgp_route_cmd);
   install_element (RESTRICTED_NODE, &show_ip_bgp_vrf_route_cmd);
+  install_element (RESTRICTED_NODE, &show_ipv6_bgp_vrf_route_cmd);
   install_element (RESTRICTED_NODE, &show_ip_bgp_ipv4_route_cmd);
   install_element (RESTRICTED_NODE, &show_ip_bgp_vpnv4_all_route_cmd);
   install_element (RESTRICTED_NODE, &show_bgp_l2vpn_evpn_all_route_cmd);
@@ -18274,9 +18300,11 @@ bgp_route_init (void)
   install_element (RESTRICTED_NODE, &show_ip_bgp_view_rsclient_prefix_cmd);
   install_element (ENABLE_NODE, &show_ip_bgp_cmd);
   install_element (ENABLE_NODE, &show_ip_bgp_vrf_cmd);
+  install_element (ENABLE_NODE, &show_ipv6_bgp_vrf_cmd);
   install_element (ENABLE_NODE, &show_ip_bgp_ipv4_cmd);
   install_element (ENABLE_NODE, &show_ip_bgp_route_cmd);
   install_element (ENABLE_NODE, &show_ip_bgp_vrf_route_cmd);
+  install_element (ENABLE_NODE, &show_ipv6_bgp_vrf_route_cmd);
   install_element (ENABLE_NODE, &show_ip_bgp_ipv4_route_cmd);
   install_element (ENABLE_NODE, &show_ip_bgp_vpnv4_all_route_cmd);
   install_element (ENABLE_NODE, &show_bgp_l2vpn_evpn_all_route_cmd);
