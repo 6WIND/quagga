c00f5c050c53844168f66aff91e1c43bb3960e81
Philippe Guibert <philippe.guibert@6wind.com>
2016-10-28 16:38:31 +0200

    bgpd: new statically configured entries in VRF RIB are silently modified
    When a change occurs in VRF RIB tables, upon adding or removing an
    entry, if this entry is locally configured, and does not come from
    remote BGP speakers, then silently add or remove entry. A debug trace
    will be displayed as before. This commit is a continuation from commit:
    " bgpd: VRF RIB log updates enhancement", where the log updates were
    introduced.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- bgpd/bgp_route.c
+++ bgpd/bgp_route.c
@@ -1675,23 +1675,6 @@ bgp_vrf_update (struct bgp_vrf *vrf, afi_t afi, struct bgp_node *rn,
   else
     event.label = 0;
 
-  if (announce == true)
-    {
-      if(CHECK_FLAG (selected->flags, BGP_INFO_UPDATE_SENT))
-        return;
-      SET_FLAG (selected->flags, BGP_INFO_UPDATE_SENT);
-      UNSET_FLAG (selected->flags, BGP_INFO_WITHDRAW_SENT);
-    }
-  else
-    {
-      /* if not already sent, do nothing */
-      if(!CHECK_FLAG (selected->flags, BGP_INFO_UPDATE_SENT))
-        return;
-      if(CHECK_FLAG (selected->flags, BGP_INFO_WITHDRAW_SENT))
-        return;
-      SET_FLAG (selected->flags, BGP_INFO_WITHDRAW_SENT);
-      UNSET_FLAG (selected->flags, BGP_INFO_UPDATE_SENT);
-    }
   if (BGP_DEBUG (events, EVENTS))
     {
       char vrf_rd_str[RD_ADDRSTRLEN], rd_str[RD_ADDRSTRLEN], pfx_str[INET6_BUFSIZ];
@@ -1718,6 +1701,28 @@ bgp_vrf_update (struct bgp_vrf *vrf, afi_t afi, struct bgp_node *rn,
         zlog_debug ("vrf[%s] %s: prefix withdrawn nh %s label %s",
                     vrf_rd_str, pfx_str, nh_str, label_str);
     }
+
+  if(selected->type == ZEBRA_ROUTE_BGP
+     && selected->sub_type == BGP_ROUTE_STATIC)
+    return;
+
+  if (announce == true)
+    {
+      if(CHECK_FLAG (selected->flags, BGP_INFO_UPDATE_SENT))
+        return;
+      SET_FLAG (selected->flags, BGP_INFO_UPDATE_SENT);
+      UNSET_FLAG (selected->flags, BGP_INFO_WITHDRAW_SENT);
+    }
+  else
+    {
+      /* if not already sent, do nothing */
+      if(!CHECK_FLAG (selected->flags, BGP_INFO_UPDATE_SENT))
+        return;
+      if(CHECK_FLAG (selected->flags, BGP_INFO_WITHDRAW_SENT))
+        return;
+      SET_FLAG (selected->flags, BGP_INFO_WITHDRAW_SENT);
+      UNSET_FLAG (selected->flags, BGP_INFO_UPDATE_SENT);
+    }
   if (afi == AFI_IP)
     {
       if (selected->attr && selected->attr->extra)
@@ -1726,6 +1731,7 @@ bgp_vrf_update (struct bgp_vrf *vrf, afi_t afi, struct bgp_node *rn,
       bgp_notify_route (vrf->bgp, &event);
 #endif /* HAVE_ZEROMQ */
     }
+
 }
 
 int
