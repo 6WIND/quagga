26a97d68b55de54ccbb46f7ecc60870cff4356b0
Philippe Guibert <philippe.guibert@6wind.com>
2016-11-18 09:39:30 +0100

    bgpd: set restart_time from capnproto API
    The graceful restart time is configurable by using capnproto API
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- bgpd/bgp.bcapnp.c
+++ bgpd/bgp.bcapnp.c
@@ -186,7 +186,7 @@ void qcapn_BGP_set(struct bgp *s, capn_ptr p)
       
     }
     {
-      /* MISSING: restart_time */
+      s->restart_time = capn_read32(p, 28);
     }
     {
       const char * notify_zmq_url;
@@ -198,6 +198,7 @@ void qcapn_BGP_set(struct bgp *s, capn_ptr p)
     s->distance_ibgp = capn_read8(p, 7);
     s->distance_local = capn_read8(p, 8);
     s->stalepath_time = capn_read32(p, 28);
+    s->restart_time = capn_read32(p, 24);
 }
 
 
