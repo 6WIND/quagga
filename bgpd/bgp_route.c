687eb88b201be7e909442277151bde9cca91da25
Philippe Guibert <philippe.guibert@6wind.com>
2017-03-03 12:02:18 +0100

    bgpd: evpn, vpn and encap adaptation when dumping statistics
    When using the show bgp ipv4 vpn statistics command, the show routine is
    adapted so as to detect that the vpn safi is requested. Then the RIB
    table is parsed, for each route distinguisher received.
    The same occurs for encap message, and l2vpn evpn message.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- bgpd/bgp_route.c
+++ bgpd/bgp_route.c
@@ -14135,6 +14135,7 @@ struct bgp_table_stats
 {
   struct bgp_table *table;
   unsigned long long counts[BGP_STATS_MAX];
+  safi_t safi;
 };
 
 #if 0
@@ -14153,17 +14154,15 @@ ravg_tally (unsigned long count, unsigned long oldavg, unsigned long newval)
 }
 #endif
 
-static int
-bgp_table_stats_walker (struct thread *t)
+static void
+bgp_table_stats_walker_internal (struct bgp_table_stats *ts, struct bgp_table *table)
 {
   struct bgp_node *rn;
-  struct bgp_node *top;
-  struct bgp_table_stats *ts = THREAD_ARG (t);
   unsigned int space = 0;
-  
-  if (!(top = bgp_table_top (ts->table)))
-    return 0;
+  struct bgp_node *top;
 
+  if (!(top = bgp_table_top (table)))
+      return;
   switch (top->p.family)
     {
       case AF_INET:
@@ -14254,9 +14253,38 @@ bgp_table_stats_walker (struct thread *t)
             }
         }
     }
-  return 0;
+  return;
 }
 
+static int
+bgp_table_stats_walker (struct thread *t)
+{
+  struct bgp_table *top;
+  struct bgp_table_stats *ts = THREAD_ARG (t);
+
+  top = ts->table;
+
+  if (ts->safi == SAFI_MPLS_VPN || ts->safi == SAFI_ENCAP ||
+      ts->safi == SAFI_EVPN)
+    {
+      struct bgp_table *table;
+      struct bgp_node *rn;
+
+      for (rn = bgp_table_top (top); rn;
+           rn = bgp_route_next (rn))
+        {
+          if ((table = rn->info) != NULL)
+            {
+              zlog_err("this way");
+              bgp_table_stats_walker_internal (ts, table);
+            }
+        }
+    }
+  else
+    bgp_table_stats_walker_internal (ts, top);
+  zlog_err("endof way");
+  return 0;
+}
 static int
 bgp_table_stats (struct vty *vty, struct bgp *bgp, afi_t afi, safi_t safi)
 {
@@ -14271,6 +14299,7 @@ bgp_table_stats (struct vty *vty, struct bgp *bgp, afi_t afi, safi_t safi)
   
   memset (&ts, 0, sizeof (ts));
   ts.table = bgp->rib[afi][safi];
+  ts.safi = safi;
   thread_execute (bm->master, bgp_table_stats_walker, &ts, 0);
 
   vty_out (vty, "BGP %s RIB statistics%s%s",
