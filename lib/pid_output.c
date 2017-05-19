b222d7b34d1af511070e81df1a15d68962aa1f13
Philippe Guibert <philippe.guibert@6wind.com>
2017-05-19 19:00:19 +0200

    lib: add proc_find pid utility
    This utility routing retrieves the process id of a process with its
    name. It checks for process IDs that are not the self process.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- lib/pid_output.c
+++ lib/pid_output.c
@@ -24,8 +24,55 @@
 #include <fcntl.h>
 #include <log.h>
 #include "version.h"
-
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/types.h>
 #define PIDFILE_MASK 0644
+
+pid_t proc_find(const char* name) 
+{
+  DIR* dir;
+  struct dirent* ent;
+  char* endptr;
+  char buf[512];
+
+  if (!(dir = opendir("/proc"))) {
+    perror("can't open /proc");
+    return -1;
+  }
+
+  while((ent = readdir(dir)) != NULL) {
+    /* if endptr is not a null character, the directory is not
+     * entirely numeric, so ignore it */
+    long lpid = strtol(ent->d_name, &endptr, 10);
+    if (*endptr != '\0') {
+      continue;
+    }
+    if (lpid == getpid())
+      continue;
+    /* try to open the cmdline file */
+    snprintf(buf, sizeof(buf), "/proc/%ld/cmdline", lpid);
+    FILE* fp = fopen(buf, "r");
+
+    if (fp) {
+      if (fgets(buf, sizeof(buf), fp) != NULL) {
+        /* check the first token in the file, the program name */
+        char* first = strtok(buf, " ");
+        if (!strcmp(first, name)) {
+          fclose(fp);
+          closedir(dir);
+          return (pid_t)lpid;
+        }
+      }
+      fclose(fp);
+    }
+
+  }
+
+  closedir(dir);
+  return -1;
+}
+
 #ifndef HAVE_FCNTL
 
 pid_t
