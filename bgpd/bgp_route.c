20e6c282ce164c523b23332835aa3e5cbc1412b8
Philippe Guibert <philippe.guibert@6wind.com>
2017-05-22 16:41:30 +0200

    bgpd: route sync does not return locally configured routes
    Locally configured routes are not looped back when SDN controller tries
    to get the route entries.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- bgpd/bgp_route.c
+++ bgpd/bgp_route.c
@@ -1656,6 +1656,9 @@ bool bgp_api_route_get (struct bgp_vrf *vrf, struct bgp_api_route *out, struct b
 
   for (sel = bn->info; sel; sel = sel->next)
     {
+      if(sel->type == ZEBRA_ROUTE_BGP
+         && sel->sub_type == BGP_ROUTE_STATIC)
+        continue;
       if (iter_on_multipath)
         {
           if (CHECK_FLAG (sel->flags, BGP_INFO_MULTIPATH))
