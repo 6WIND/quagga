a54a1130207c6630be82d799020be448692dcfa6
Philippe Guibert <philippe.guibert@6wind.com>
2017-07-05 14:40:05 +0200

    qthriftd: before closing 7644 connection, flush the socket
    In order to be sure that the TCP connection will be correctly closed,
    use a flush before.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- qthriftd/qthrift_vpnservice.c
+++ qthriftd/qthrift_vpnservice.c
@@ -402,6 +402,13 @@ void qthrift_vpnservice_terminate_client(struct qthrift_vpnservice_client *peer)
   if(peer == NULL)
     return;
   /* peer destroy */
+  thrift_transport_flush(peer->transport, &error);
+  if (error != NULL)
+    {
+      zlog_err("Unable to flush thrift socket: %s\n", error->message);
+      g_error_free (error);
+      error = NULL;
+    }
   thrift_transport_close(peer->transport, &error);
   if (error != NULL)
     {
