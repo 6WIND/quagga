aac0624904f7637e37ac39141cf31117609abe82
Philippe Guibert <philippe.guibert@6wind.com>
2017-07-05 12:38:04 +0200

    qthriftd: display thrift error when attempting to close 7644 connection
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- qthriftd/qthrift_vpnservice.c
+++ qthriftd/qthrift_vpnservice.c
@@ -481,10 +481,16 @@ void qthrift_vpnservice_set_thrift_bgp_updater_client_port (struct qthrift_vpnse
 
 void qthrift_vpnservice_terminate_client(struct qthrift_vpnservice_client *peer)
 {
+  GError *error = NULL;
   if(peer == NULL)
     return;
   /* peer destroy */
-  thrift_transport_close(peer->transport, NULL);
+  thrift_transport_close(peer->transport, &error);
+  if (error != NULL)
+    {
+      zlog_err("Unable to close thrift socket: %s\n", error->message);
+      g_error_free (error);
+    }
   g_object_unref(peer->transport_buffered);
   g_object_unref(peer->protocol);
   peer->protocol = NULL;
