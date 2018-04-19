8dd22143da99a48e8d38421df27c8fdf534e818c
Xiaofeng Liu <xiaofeng.liu@6wind.com>
2018-04-19 10:54:59 +0200

    bgpd: use correct prefix rd to lookup global rib table
    When a vrf is deleted, the routes imported by the vrf should be
    withdrawn, and the corresponding route entry in global rib should
    be marked as hidden if no other vrf imports this route.
    
    When global rib is searched, original prd attached in bgp_info
    (vrf_ri->extra->vrf_rd) should be used, instead of this vrf's
    outbound_rd.
    
    Also, ri will be used in bgp_vrf_delete_vrf_update_global_rib(),
    so bgp_info_reap() is called after global rib is updated.
    
    Signed-off-by: Xiaofeng Liu <xiaofeng.liu@6wind.com>

--- bgpd/bgp_route.c
+++ bgpd/bgp_route.c
@@ -1638,8 +1638,8 @@ void bgp_vrf_clean_tables (struct bgp_vrf *vrf)
           {
             ri_next = ri->next;
             bgp_vrf_update(vrf, afi, rn, ri, false);
-            bgp_info_reap (rn, ri);
             bgp_vrf_delete_vrf_update_global_rib (&(rn->p), ri, vrf, afi);
+            bgp_info_reap (rn, ri);
           }
       bgp_table_finish (&vrf->rib[afi]);
 
@@ -2463,7 +2463,7 @@ bgp_vrf_delete_vrf_update_global_rib (struct prefix *p, struct bgp_info *vrf_ri,
   size_t i;
   int vrf_ignore = 1;
   struct attr *attr;
-  struct prefix_rd *prd = &(in_vrf->outbound_rd);
+  struct prefix_rd *prd = &(vrf_ri->extra->vrf_rd);
 
   bgp = bgp_get_default ();
   if (bgp == NULL)
