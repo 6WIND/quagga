e1eb41595c5e869417123646a9648269562c4250
Git@core <git-core@6wind.com>
2019-08-19 17:19:27 +0200

    bgpd: withdraw log message is dumping prefix evpn properly
    withdraw log message is dumping prefix evpn properly.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- bgpd/bgp_packet.c
+++ bgpd/bgp_packet.c
@@ -461,12 +461,12 @@ bgp_withdraw_packet (struct peer *peer, afi_t afi, safi_t safi)
 
       if (BGP_DEBUG (update, UPDATE_OUT))
         {
-          char buf[INET6_BUFSIZ];
+          char buf[PREFIX_STRLEN];
 
-          zlog (peer->log, LOG_DEBUG, "%s send UPDATE %s/%d -- unreachable",
-                peer->host,
-                inet_ntop (rn->p.family, &(rn->p.u.prefix), buf, INET6_BUFSIZ),
-                rn->p.prefixlen);
+          prefix2str(&rn->p, buf, sizeof(buf));
+          zlog (peer->log, LOG_DEBUG, "%s send UPDATE %s -- unreachable",
+                peer->host, buf);
+
         }
 
       peer->scount[afi][safi]--;
